d35e79d01976403a21f14bbf36a281259ec954c1
Omego2K <omego2k@gmail.com>
2019-12-29 15:04:14 -0500

    run code cleanup

--- core/AlphaDev.Paging/Extensions/UInt32Extensions.cs
+++ core/AlphaDev.Paging/Extensions/UInt32Extensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using AlphaDev.Paging.Support;
 
 namespace AlphaDev.Paging.Extensions
--- tests/unit/AlphaDev.Paging.Tests.Unit/Extensions/EnumerableExtensionsTests.cs
+++ tests/unit/AlphaDev.Paging.Tests.Unit/Extensions/EnumerableExtensionsTests.cs
@@ -10,28 +10,6 @@ namespace AlphaDev.Paging.Tests.Unit.Extensions
     public class EnumerableExtensionsTests
     {
         [Fact]
-        public void ToPagerReturnsPagerItemsInTheSameOrder()
-        {
-            var items = Enumerable.Range(1, 10).ToArray();
-            var pager = items.ToPager(5, () => 101);
-            pager.Should().BeEquivalentTo(items, options => options.WithStrictOrdering());
-        }
-
-        [Fact]
-        public void ToPagerReturnsPagerWithCurrentPage()
-        {
-            var pager = new object[10].ToPager(5, () => 101);
-            pager.Pages.Current.Should().Be(5);
-        }
-
-        [Fact]
-        public void ToPagerReturnsPagerWithLastPageBasedOnTotalItemsAndItemsPerPage()
-        {
-            var pager = new object[10].ToPager(5, () => 101);
-            pager.Pages.Last.Should().Be(11);
-        }
-
-        [Fact]
         public async Task ToPagerAsyncReturnsPagerItemsInTheSameOrder()
         {
             var items = Enumerable.Range(1, 10).ToArray();
@@ -106,6 +84,28 @@ namespace AlphaDev.Paging.Tests.Unit.Extensions
                    .Throw<InvalidOperationException>()
                    .WithMessage(
                        "Invalid amount of items. Expected 10, but was 1. Only the last page may contain less than 10 items.");
+        }
+
+        [Fact]
+        public void ToPagerReturnsPagerItemsInTheSameOrder()
+        {
+            var items = Enumerable.Range(1, 10).ToArray();
+            var pager = items.ToPager(5, () => 101);
+            pager.Should().BeEquivalentTo(items, options => options.WithStrictOrdering());
+        }
+
+        [Fact]
+        public void ToPagerReturnsPagerWithCurrentPage()
+        {
+            var pager = new object[10].ToPager(5, () => 101);
+            pager.Pages.Current.Should().Be(5);
+        }
+
+        [Fact]
+        public void ToPagerReturnsPagerWithLastPageBasedOnTotalItemsAndItemsPerPage()
+        {
+            var pager = new object[10].ToPager(5, () => 101);
+            pager.Pages.Last.Should().Be(11);
         }
 
         [Fact]
